ed91a8bbbabe335f1b751ac75baaf4e8fd394d7e
Muhamad Taopik <mr.94t3z@gmail.com>
2024-03-28 16:30:51 +0700

    ✅ support open frames

--- api/index.tsx
+++ api/index.tsx
@@ -93,24 +93,50 @@ export const app = new Frog({
 // Support Open Frames
 app.use(async (c, next) => {
 
-  console.log('Incoming request:', c.req);
-
   await next();
   const isFrame = c.res.headers.get('content-type')?.includes('html');
-
   if (isFrame) {
     let html = await c.res.text();
-    const metaTag = '<meta property="of:accepts:xmtp" content="2024-02-01" />';
-    html = html.replace(/(<head>)/i, `$1${metaTag}`);
+    const regex = /<meta.*?\/>/gs;
+    const matches = [...html.matchAll(regex)];
+    let metaTags = matches.map((match) => match[0])?.join?.('');
+    /*
+    of:image    fc:frame:image
+    og:image    og:image
+    of:button:$idx    fc:frame:button:index
+    of:button:$idx:action    fc:frame:button:$idx:action
+    of:button:$idx:target    fc:frame:button:$idx:target
+    of:input:text    fc:frame:input:text
+    of:image:aspect_ratio    fc:frame:image:aspect_ratio
+    of:accepts:farcaster    fc:frame
+    of:state    fc:frame:state
+    */
+    // Complete replacements according to the mapping provided
+    let openFrameTags = metaTags
+      .replaceAll('fc:frame:image', 'of:image')
+      // Assuming a pattern for of:button:$idx replacements
+      .replace(/fc:frame:button:(\d+)/g, 'of:button:$1')
+      .replace(/fc:frame:button:(\d+):action/g, 'of:button:$1:action')
+      .replace(/fc:frame:button:(\d+):target/g, 'of:button:$1:target')
+      // Additional replacements based on the provided pattern
+      .replaceAll('fc:frame:input:text', 'of:input:text')
+      .replaceAll('fc:frame:image:aspect_ratio', 'of:image:aspect_ratio')
+      .replaceAll('fc:frame:state', 'of:state');
+
+    openFrameTags += [
+      `<meta property="of:accepts:farcaster" content="vNext"/>`,
+      `<meta property="of:accepts:xmtp" content="2024-02-01"/>`,
+      `<meta property="of:accepts:lens" content="1.1"/>`,
+    ].join('\n');
+
+    html = html.replace(/(<head>)/i, `$1${openFrameTags}`);
+
     c.res = new Response(html, {
       headers: {
         'content-type': 'text/html',
       },
     });
   }
-
-
-  console.log('Outgoing response:', c.res);
 });
 
 
