50684bc70ea39874ce35325b69c89ba23acbb7f9
Muhamad Taopik <mr.94t3z@gmail.com>
2024-03-28 13:55:52 +0700

    ✅ support open frames

--- api/index.tsx
+++ api/index.tsx
@@ -90,6 +90,29 @@ export const app = new Frog({
   basePath: '/api/frame',
 });
 
+// Support Open Frames
+app.use(async (c, next) => {
+
+  console.log('Incoming request:', c.req);
+
+  await next();
+  const isFrame = c.res.headers.get('content-type')?.includes('html');
+
+  if (isFrame) {
+    let html = await c.res.text();
+    const metaTag = '<meta property="of:accepts:xmtp" content="2024-02-01" />';
+    html = html.replace(/(<head>)/i, `$1${metaTag}`);
+    c.res = new Response(html, {
+      headers: {
+        'content-type': 'text/html',
+      },
+    });
+  }
+
+
+  console.log('Outgoing response:', c.res);
+});
+
 // Initial frame
 app.frame('/', (c) => {
   currentPage = 1;
